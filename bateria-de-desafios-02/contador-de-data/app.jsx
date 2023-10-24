f3ba067d81fca119cb8af430cd7a55f4146f6d4a
Jansen Muniz <jansen.muniz1@gmail.com>
2023-10-24 18:14:52 -0300

    alteração

--- bateria-de-desafios-02/contador-de-data/app.jsx
+++ bateria-de-desafios-02/contador-de-data/app.jsx
@@ -1,20 +1,20 @@
-const buttonMinusInterval = () => console.log('Clicou no menos do Intervalo')
-const buttonAddInterval = () => console.log('Clicou no mais do Intervalo')
-const buttonMinusStep = () => console.log('Clicou no menos da Contagem')
-const buttonAddStep = () => console.log('Clicou no mais da Contagem')
+const handleClickMinusInterval = () => console.log('Clicou no menos do Intervalo')
+const handleClickAddInterval = () => console.log('Clicou no mais do Intervalo')
+const handleClickMinusStep = () => console.log('Clicou no menos da Contagem')
+const handleClickAddStep = () => console.log('Clicou no mais da Contagem')
 
 const App = () => (
   <div className="container">
     <div className="count">
-      <button onClick={buttonMinusInterval}>-</button>
+      <button onClick={handleClickMinusInterval}>-</button>
       <h2>Intervalo: 1</h2>
-      <button onClick={buttonAddInterval}>+</button>
+      <button onClick={handleClickAddInterval}>+</button>
     </div>
 
     <div className="count">
-      <button onClick={buttonMinusStep}>-</button>
+      <button onClick={handleClickMinusStep}>-</button>
       <h2>Contagem: 0</h2>
-      <button onClick={buttonAddStep}>+</button>
+      <button onClick={handleClickAddStep}>+</button>
     </div>
     <h2>Hoje é Domingo, 22 de out. de 2023</h2>
   </div>
